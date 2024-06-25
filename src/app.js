ea1e6658336f81d304a301df871ed54386fdaeae
macd26 <marieannedoherty@mac.com>
2024-06-25 17:29:33 +1000

    Injected the forecast HTML from Java S

--- src/app.js
+++ src/app.js
@@ -54,26 +54,24 @@ function displayForecast() {
   let forecastHtml = "";
 
   days.forEach(function (day) {
-    forecast.innerHtml =
+    forecastHtml =
       forecastHtml +
       `
-  <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">⛅️</div>
-    <div class="weather-forecast-temperatures">
-      <div class="weather-forecast-temperature">
-        <strong>15°</strong>
-      </div>
-      <div class="weather-forecast-temperature">9°</div>
-     </div>   
+<div class="weather-forecast-day">
+  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-icon">⛅️</div>
+  <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature">
+      <strong>15°</strong>
+    </div>
+    <div class="weather-forecast-temperature">9°</div>
   </div>
+</div>
 `;
   });
-
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
